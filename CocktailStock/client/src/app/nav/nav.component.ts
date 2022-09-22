2824af2ffa6eb9a384f17a17d0c89c209bb7cd98
Lucas-Martinez-Sol <lucasmp@outlook.fr>
2022-09-22 14:25:27 +0200

    End 220/265

--- CocktailStock/client/src/app/nav/nav.component.ts
+++ CocktailStock/client/src/app/nav/nav.component.ts
@@ -10,6 +10,7 @@ import { AccountService } from '../_services/account.service';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
+
 export class NavComponent implements OnInit {
 
   model: any = {}
